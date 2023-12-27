741ad672aca1a2c1dc7c30131abf02a0cee9d11d
Ismael Pedro <http.ismaeel@gmail.com>
2023-12-26 23:39:03 -0300

    currentCustomer

--- lib/design_system.dart
+++ lib/design_system.dart
@@ -1,26 +1,22 @@
 library design_system;
 
-import 'package:design_system/presenter/app_assets.dart';
 import 'package:design_system/presenter/app_colors.dart';
 import 'package:flutter/material.dart';
 
 enum Customer { voomp, anhaguera, stoodi }
 
-Customer customer = Customer.voomp;
+Customer currentCustomer = Customer.voomp;
 
 void configureCustomer(Customer customer) {
+  currentCustomer = Customer.voomp;
   switch (customer) {
     case Customer.voomp:
-      debugPrint(AppAssets.logo);
-      customer = Customer.voomp;
       AppColors.primary = const Color(0xfffe8700);
       break;
     case Customer.stoodi:
-      customer = Customer.stoodi;
       AppColors.primary = const Color(0xff004efe);
       break;
     case Customer.anhaguera:
-      customer = Customer.anhaguera;
       AppColors.primary = const Color(0xffff5424);
       break;
   }
